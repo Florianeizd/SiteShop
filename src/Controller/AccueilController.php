505858f7c1d19ab5784dc2f6977cf1508ade53ad
Floriane Izard <floriane.izd45@gmail.com>
2021-12-09 19:01:15 +0100

    modification 09/12

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,25 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
-use App\Entity\Article;
-use App\Repository\ArticleRepository;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'accueil')]
-    public function index(ArticleRepository $repo): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
 
-        $articles = $repo->findAll();
+        $articles = $articleRepository->findAll();
 
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+    
             'articles' => $articles
         ]);
     }
@@ -36,26 +37,35 @@ class AccueilController extends AbstractController
 
 
     #[Route('/accueil/new', name: 'accueil_create')]
-    public function create(Request $request){
-        $article = new Article();
+    #[Route('/accueil/{id}/edit', name: 'accueil_edit')]
 
-        $form = $this->createFormBuilder($article)
-                     ->add('nom')
-                     ->add('description')
-                     ->add('prix')
-                     ->getForm();
+    public function form(Article $article = null, Request $request, EntityManagerInterface $manager){ //paramconverter:convertit un paramètre en une entité
+        
+        if(!$article){
+            $article = new Article();
+        }
+     
+        // $form = $this->createFormBuilder($article) //champs qui sont relié a une entité article 
+        //              ->add('nom')
+        //              ->add('description')
+        //              ->add('prix')
+        //              ->getForm();
 
-        $form->handleRequest($request);
+        $form = $this->createForm(ArticleType::class, $article); 
 
-        if($form->isSubmitted() && $form->isValid()){
-            // $manager->persist($article);
-            // $manager->flush();
+        $form->handleRequest($request); //analyse de la requête
 
-            // return $this->redirectToRoute('accueil_show', ['id'=> $article->getId()]);
+        if($form->isSubmitted() && $form->isValid()){ //si c'est submis et valide
+        
+            $manager->persist($article); //enregistrer dans la bdd
+            $manager->flush();
+
+            return $this->redirectToRoute('article_show', ['id'=> $article->getId()]); //redirection de la page d'article 
         }
 
         return $this->render('accueil/create.html.twig', [
-            'formArticle' => $form->createView()
+            'formArticle' => $form->createView(), //form
+            'editMode' =>$article->getId() !== null //si il est diff de null = true donc on sera en editMode
         ]);
     }
 
@@ -66,7 +76,5 @@ class AccueilController extends AbstractController
         return $this->render('accueil/show.html.twig', [
             'article' => $article ]);
     }
-
-    
 
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,7 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
@@ -19,18 +22,37 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=3, max=255)
      */
     private $nom;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\Length(min=3, max=255)
      */
     private $description;
 
     /**
      * @ORM\Column(type="decimal", precision=10, scale=0)
+     * @Assert\Range(min=1, max=255)
      */
     private $prix;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Categorie::class, inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Avis::class, mappedBy="article", orphanRemoval=true)
+     */
+    private $avis;
+
+    public function __construct()
+    {
+        $this->avis = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -69,6 +91,48 @@ class Article
     public function setPrix(string $prix): self
     {
         $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Avis[]
+     */
+    public function getAvis(): Collection
+    {
+        return $this->avis;
+    }
+
+    public function addAvi(Avis $avi): self
+    {
+        if (!$this->avis->contains($avi)) {
+            $this->avis[] = $avi;
+            $avi->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAvi(Avis $avi): self
+    {
+        if ($this->avis->removeElement($avi)) {
+            // set the owning side to null (unless already changed)
+            if ($avi->getArticle() === $this) {
+                $avi->setArticle(null);
+            }
+        }
 
         return $this;
     }
